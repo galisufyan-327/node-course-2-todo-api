5ddec61de0f69ccaac1084118c39daacf10f7fdc
Ali Sufyan <alisufyan327@gmail.com>
2018-08-07 17:16:46 -0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,26 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            var hashedPassword = user.password;
+            bcrypt.compare(password, hashedPassword, (err, result) => {
+                if (result) {
+                    resolve(user);
+                }
+                reject();
+            });
+        });
+    });
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -105,6 +106,34 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var email = body.email;
+    var password = body.password;
+    
+    User.findByCredentials(email, password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(404).send(e);
+    });
+
+    // var email = req.body.email;
+    // var password = req.body.password;
+
+    // User.findOne({email}).then((user) => {
+    //     var hashedPassword = user.password;
+    //     bcrypt.compare(password, hashedPassword, (err, result) => {
+    //         if (result) {
+    //             res.send(user.tokens[0].token);
+    //         }
+    //     })
+    // }).catch((e) => {
+    //     res.status(404).send(e);
+    // });
 });
 
 app.get('/users/me', authenticate, (req, res) => {
